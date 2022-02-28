5497f327e8a976dc1980ec3212a2e3ff92642039
sergio figueroa <figueroa.sergio.26@gmail.com>
2022-02-28 20:47:25 +0100

    add properties

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+//  la api key solo es validad  y utilizable en el modulo
 import { ProductsModule } from './modules/products/products.module';
-
+const API_KEY = '1234324';
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, { provide: 'APY_KEY', useValue: API_KEY }],
   imports: [ProductsModule],
 })
 export class AppModule {}
